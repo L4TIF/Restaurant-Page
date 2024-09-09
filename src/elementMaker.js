5d6b383065798639948f09b2c97d3f2410ce300c
Latiful Rahman <latif.fiverr@gmail.com>
2024-09-09 16:36:30 +0000

    menu page created

--- src/elementMaker.js
+++ src/elementMaker.js
@@ -1,10 +1,7 @@
 export const makeElement = (elementType, classesString, id) => {
     let element = document.createElement(elementType);
     let classArr = classesString.split(" ");
-    console.log(classArr)
     element.classList.add(...classArr);
-
-
     element.id = id;
     return element;
 }
