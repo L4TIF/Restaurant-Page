577c657c72c938712b87247c218e3cf263148dcf
Latiful Rahman <latif.fiverr@gmail.com>
2024-09-12 15:40:19 +0000

    minor changes

--- src/home.js
+++ src/home.js
@@ -12,7 +12,7 @@ const home = () => {
 
 
     // hero text element creation
-    let heroText = makeElement("div", "text-wrap py-16 text-7xl text-white lg:px-16 lg:text-8xl", "hero-text");
+    let heroText = makeElement("div", "text-wrap pt-12 lg:py-12 text-7xl text-white lg:px-16 lg:text-8xl", "hero-text");
     let heroTextTop = makeElement("div", "animate__animated animate__fadeInUp animate__delay-1s font-thin", "hero-text-top");
     let heroTextBottom = makeElement("div", "animate__animated animate__fadeInUp animate__delay-2s font-thin sm:ml-16 md:my-10 lg:ml-44", "hero-text-bottom")
     let spanTop = makeElement("span", "font-bold", "spanTop");
@@ -38,7 +38,7 @@ const home = () => {
 
     // hero pic
     let heroPic = makeElement("div", "animate__animated animate__fadeIn animate__delay-1s row-span-2 flex max-w-full items-center align-top", "hero-pic");
-    let heroImg = makeElement("img", "block drop-shadow-9xl sm:max-w-lg xl:max-w-3xl object-cover pl-8 lg:pl-0");
+    let heroImg = makeElement("img", "block drop-shadow-9xl sm:max-w-lg xl:max-w-3xl object-cover pl-6 mb-10 lg:mb-0 lg:pl-0");
     heroImg.src = salad;
     heroImg.alt = "salad";
     heroPic.appendChild(heroImg);     // append
@@ -46,7 +46,7 @@ const home = () => {
     // hero desc
     let heroDesc = makeElement("div", "relative animate__animated animate__fadeInUp animate__delay-3s relative md:pr-[36%]", "hero-desc");
     let heroDescText = makeElement("div", "relative h-fit bg-white p-16 text-lg", "hero-desc-text");
-    let recipeBtn = makeElement("div", "absolute -bottom-10 left-16 lg:bottom-10 lg:left-36 flex h-20 w-72 items-center justify-between bg-yellow-500 px-12 shadow-2xl hover:bg-yellow-400", "recipe");
+    let recipeBtn = makeElement("div", "absolute -bottom-10 left-16 lg:bottom-6 lg:left-36 flex h-20 w-72 items-center justify-between bg-yellow-500 px-12 shadow-2xl hover:bg-yellow-400", "recipe");
     let recipeBtnText = makeElement("div", "text-lg", "recipe-text");
     let recipeBtnArrow = makeElement("div", "text-4xl", "arrow");
 
@@ -73,7 +73,7 @@ const home = () => {
     main.appendChild(heroPic);
     main.appendChild(heroDesc);
 
-    recipeBtn.addEventListener("click",recipe);
+    recipeBtn.addEventListener("click", recipe);
 }
 
 
