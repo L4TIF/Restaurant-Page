4053d5008d51ca68fb0ba91c185ce1e72d02b704
Latiful Rahman <latif.fiverr@gmail.com>
2024-09-12 16:37:33 +0000

    hamburger done

--- src/index.js
+++ src/index.js
@@ -4,13 +4,32 @@ import recipe from './recipe.js';
 import contact from './contact.js';
 
 home(); //initializing first time
-let icon = document.getElementById("logo");
+const icon = document.getElementById("logo");
+
 let homeBTn = document.getElementById("home");
 let menuBtn = document.getElementById("recipe");
 let contactBtn = document.getElementById("contact-us");
 
-icon.addEventListener("click",home);
-homeBTn.addEventListener("click",home);
-menuBtn.addEventListener("click",recipe);
-contactBtn.addEventListener("click",contact);
+const menubar = document.getElementById("menu-bar");
+const hamburgerIcon = document.getElementById("hamburger-icon");
 
+icon.addEventListener("click", home);
+
+
+
+homeBTn.addEventListener("click", home);
+menuBtn.addEventListener("click", recipe);
+contactBtn.addEventListener("click", contact);
+
+
+menubar.addEventListener("click", () => {
+    hamburgerIcon.classList.toggle("hidden");
+    if (!hamburgerIcon.classList.contains("hidden")) {
+        homeBTn = document.getElementById("hamburger-home");
+        menuBtn = document.getElementById("hamburger-menu");
+        contactBtn = document.getElementById("hamburger-contact-us");
+    }
+    homeBTn.addEventListener("click", home);
+    menuBtn.addEventListener("click", recipe);
+    contactBtn.addEventListener("click", contact);
+})
